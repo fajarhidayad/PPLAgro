e35a7702cdff177487ca8af27033893a1e36cff4
fajarhidayad <fajarsuryahidayad@gmail.com>
2018-10-22 20:50:52 +0700

    beda musuh & shop

--- Assets/PlaceMonster.cs
+++ Assets/PlaceMonster.cs
@@ -5,13 +5,19 @@ public class PlaceMonster : MonoBehaviour
 {
 
     public GameObject monsterPrefab;
+    public GameObject monsterPrefab2;
     private GameObject monster;
+    private GameObject monster2;
     private GameManagerBehavior gameManager;
+    Shop shop;
+
+    //public static int turets = Shop.getTurret();
 
     // Use this for initialization
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+        //turets = 1;
     }
 
     // Update is called once per frame
@@ -22,31 +28,64 @@ public class PlaceMonster : MonoBehaviour
 
     private bool CanPlaceMonster()
     {
+
         int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
         return monster == null && gameManager.Gold >= cost;
+    }
+
+    private bool CanPlaceMonster2()
+    {
+
+        int cost = monsterPrefab2.GetComponent<MonsterData>().levels[0].cost;
+        return monster2 == null && gameManager.Gold >= cost;
     }
 
     //1
     void OnMouseUp()
     {
         //2
-        if (CanPlaceMonster())
+       // Debug.Log(turets);
+        if (Shop.getTurret()== 1)
         {
-            //3
-            monster = (GameObject) Instantiate(monsterPrefab, transform.position, Quaternion.identity);
-            //4
-            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-            audioSource.PlayOneShot(audioSource.clip);
+            if (CanPlaceMonster())
+            {
+                //3
+                monster = (GameObject)Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+                //4
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(audioSource.clip);
 
-            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+                gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+            }
+            else if (CanUpgradeMonster())
+            {
+                monster.GetComponent<MonsterData>().IncreaseLevel();
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(audioSource.clip);
+
+                gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+            }
         }
-        else if (CanUpgradeMonster())
+        else if (Shop.getTurret() == 2)
         {
-            monster.GetComponent<MonsterData>().IncreaseLevel();
-            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-            audioSource.PlayOneShot(audioSource.clip);
+            if (CanPlaceMonster2())
+            {
+                //3
+                monster2 = (GameObject)Instantiate(monsterPrefab2, transform.position, Quaternion.identity);
+                //4
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(audioSource.clip);
 
-            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+                gameManager.Gold -= monster2.GetComponent<MonsterData>().CurrentLevel.cost;
+            }
+            else if (CanUpgradeMonster2())
+            {
+                monster2.GetComponent<MonsterData>().IncreaseLevel();
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.PlayOneShot(audioSource.clip);
+
+                gameManager.Gold -= monster2.GetComponent<MonsterData>().CurrentLevel.cost;
+            }
         }
     }
 
@@ -55,6 +94,20 @@ public class PlaceMonster : MonoBehaviour
         if (monster != null)
         {
             MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.GetNextLevel();
+            if (nextLevel != null)
+            {
+                return gameManager.Gold >= nextLevel.cost;
+            }
+        }
+        return false;
+    }
+
+    private bool CanUpgradeMonster2()
+    {
+        if (monster2 != null)
+        {
+            MonsterData monsterData = monster2.GetComponent<MonsterData>();
             MonsterLevel nextLevel = monsterData.GetNextLevel();
             if (nextLevel != null)
             {
---
+++ Assets/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public static int turret;
+
+	public void PurchaseStandardTurret(){
+        turret = 1;
+        Debug.Log("Standard Turret Purchased"+turret);
+        
+    }
+
+    public void PurchaseAnotherTurret()
+    {
+        turret = 2;
+        Debug.Log("Another Turret Purchased"+turret);
+        
+    }
+
+    public static int getTurret() {
+        return turret; 
+    }
+}
