7b34695c690affe4df442b6c90e05882eaf2ef48
fajarhidayad <fajarsuryahidayad@gmail.com>
2018-11-22 20:16:52 +0700

    perbarui UI

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour {
 
     //public string levelToLoad = "LevelSelect";
+    public GameObject ui;
+    public GameObject ui2;
     public SceneFader sceneFader;
 
     public void Play() {
         sceneFader.FadeTo("Level");
     }
 
-    
+    public void Bantuan() {
+        ui.SetActive(!ui.activeSelf);
+    }
+
+    public void Tentang()
+    {
+        ui2.SetActive(!ui2.activeSelf);
+    }
 
     public void Quit() {
         Application.Quit();
